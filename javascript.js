2f704b1d4e2ae6252a6f3943ad4ca313940b5933
Matthew Webb <mwebb2321@gmail.com>
2021-01-19 11:05:58 -0800

    wasn't happy with where I was at. Started over

--- javascript.js
+++ javascript.js
@@ -1,28 +1,11 @@
-function countDown(num) {
-    for (var i = num; i > 0; i--) {
-      console.log(i);
-    }
+//Timer
+var count = 15;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    alert("Time is up!");
   }
-
-document.getElementById("app").innerHTML = `
-<div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
-    </g>
-  </svg>
-  <span>
-    ${formatTime(timeLeft)}
-  </span>
-</div>
-`;
-
-function formatTimeLeft(time) {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-  
-    return `${minutes}:${seconds}`;
-  }
+}, 1000);
