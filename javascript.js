8db624d0f2d2f0fa3e2af26e1b83e0a15c93e344
Matthew Webb <mwebb2321@gmail.com>
2021-01-19 16:55:34 -0800

    added html for questions

--- javascript.js
+++ javascript.js
@@ -9,3 +9,32 @@ var interval = setInterval(function(){
     alert("Time is up!");
   }
 }, 1000);
+
+document.getElementById("next").addEventListener("click", nextQuestion)
+
+var questions = [
+	{
+		prompt: "What color are apples?\n(a) Red/Green\n(b) Black\n(c) Purple",
+		answer: "a"
+	},
+	{
+		prompt: "What color are bananas?\n(a) Black\n(b) Blue\n(c) Yellow",
+		answer: "c"
+	},
+	{
+		prompt: "What color are straberries?\n(a) Yellow\n(b) Red\n(c) Blue",
+		answer: "b"
+	}
+]
+var score = 0;
+
+	for(var i = 0; i < questions.length; i++){
+		var response = window.prompt(questions[i].prompt);
+	if (response == questions[i].answer) {
+		score++;
+		alert("Correct!");
+	}	else {
+		alert("Wrong!");
+	}
+}
+alert("You got " + score + "/" + questions.length);
